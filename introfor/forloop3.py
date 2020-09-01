1e8b3752f223360e18554dc46e8c5817f73aeb7f
anthony student <fake@fake.com>
2020-09-01 21:53:28 +0000

    studying for logic

---
+++ introfor/forloop3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# create a list of strings
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+print( farms.keys() )
+
+print( farms.values() )
+
