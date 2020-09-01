c442a267c423fdf168b795d8e7b09e7a3af2353b
anthony student <fake@fake.com>
2020-09-01 22:31:04 +0000

    Loop techniques

--- introfor/forloop3.py
+++ introfor/forloop3.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python3
 # create a list of strings
-farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+farms = [
+        {"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
          {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
-         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+         {"name": "SE Farm", "agriculture": ["hens", "carrots", "celery"]}
+         ]
 
-print( farms.keys() )
+# cycle through the list of farms
+for farm in farms:
+    print(farm.get("name")+": ", end="")
+    
+    for animal in farm.get("agriculture"):
+        print(animal, end=" ")
+    print()          
+#for animals in farms:
+#    print(animals.get("agriculture"))
 
-print( farms.values() )
 
+print()
+print()
+
+for farm in farms:
+    print(farm.get("name") + ": ", end="")
+    animals = farm.get("agriculture")
+    
+    # unpacking
+    print(*animals, sep=" ")
+    print(*animals, sep=" Ei Ei Oh ")
+
+
+
+print()
+print()
+
+for farm in farms:
+    print(farm.get("name") + ": ", end="")
+    print(" ".join(farm.get("agriculture")))
